41e18f223a52404147fb8570e028bad4b98c0486
vitor <vitor.holler@outlook.pt>
2020-11-24 15:47:26 -0300

    Feat: Inicio criação de Usuário

---
+++ src/models/FirebaseModel.js
@@ -0,0 +1,80 @@
+const admin = require("firebase-admin");
+const firebase = require("firebase/app");
+
+require("firebase/auth");
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DATABASEURL
+});
+
+module.exports = {
+  async createNewUser(email, password) {
+    try {
+      const result = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+      return result.user.uid;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async createSession(email, password) {
+    try {
+      const result = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+      return result.user.uid;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async deleteUser(uid) {
+    return new Promise((resolve, reject) => {
+      admin
+        .auth()
+        .deleteUser(uid)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((error) => {
+          console.log(error);
+          const errorMessage = error.message;
+          reject(errorMessage);
+        });
+    });
+  },
+
+  async changeUserPassword(uid, newPassword) {
+    return new Promise((resolve, reject) => {
+        firebase.auth().updateUser(uid, {
+        password: newPassword
+        })
+        .then((result) => {
+            resolve(result);
+        })
+        .catch((error) => {
+            console.log(error);
+            const errorMessage = error.message;
+            reject(errorMessage);
+        })
+    })
+},
+};
