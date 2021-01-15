544cac98fc1e59f08e513aeb8f95678ef796ce9e
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-01-15 10:25:39 -0300

    FIX: formatting

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const routes = require('./routes');
-const firebase = require('firebase');
-const cors = require('cors');
-const {errors} = require('celebrate')
+require("dotenv").config();
+const express = require("express");
+const routes = require("./routes");
+const firebase = require("firebase");
+const cors = require("cors");
+const { errors } = require("celebrate");
 const port = process.env.PORT || 6969;
-
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use(routes);
 
 app.use(errors());
 
-app.listen(port,()=>{
-    console.log("Listening on port: " + port);
-  });
-
-
-
-
+app.listen(port, () => {
+  console.log("Listening on port: " + port);
+});
