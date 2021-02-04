90b19f8b9cd0b1b7780907f95f545aef6d194309
Paulo Carellos <49373945+paulohcarellos@users.noreply.github.com>
2021-02-04 14:25:23 -0300

    Arrumei

--- src/models/LiveModel.js
+++ src/models/LiveModel.js
@@ -1,36 +1,36 @@
-const { Segments, Joi } = require("celebrate");
+const connection = require("../database/connection");
 
-const liveValidator = {};
+module.exports = {
+    async createNewLive(live) {
+        try {
+            const response = await connection("live").insert(live);
+            return response;
+        }
+        catch (error) {throw new Error("Erro");}
+    },
 
-liveValidator.create = {
-  [Segments.BODY]: Joi.object().keys({
-    title: Joi.string().required(),
-    start_date: Joi.date().required(),
-    description: Joi.string().required(),
-    live_link: Joi.string().required(),
-    duration: Joi.string().required(),
-    confirmation_code: Joi.string().required(),
-  }),
-};
+    async getById(id) {
+        try {
+            const response = await connection('live').where('id', id).select('*');
+            return response;
+        } catch (error) {
+            console.log(error.message);
+            return error;
+        }
+    },
 
-liveValidator.uptade = {
-  [Segments.PARAMS]: Joi.object().keys({
-    live_id: Joi.string().required(),
-  }),
-  [Segments.BODY]: Joi.object().keys({
-      title: Joi.string().optional(),
-      start_date: Joi.date().optional(),
-      description: Joi.string().optional(),
-      live_link: Joi.string().optional(),
-      duration: Joi.string().optional(),
-      confirmation_code: Joi.string().optional(),
-    }),
-};
-
-liveValidator.deletelive = {
-  [Segments.PARAMS]: Joi.object().keys({
-    live_id: Joi.string().required(),
-  }),
-};
-
-module.exports = liveValidator;
+    async deleteLive(id) {
+        try {
+            const response = await connection("live").where("id", id).del();
+            return response;
+        }
+        catch (error) {
+            console.log(error.message);
+            return error;
+        }
+    },
+    async updateLive(id, live) {
+        const response = await connection("live").where("id", id).update(live);
+        return response;
+    }
+}
