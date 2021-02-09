fe2ebad48cabc3c3b6c8891eeb1fe62e96784f3a
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-02-09 20:20:57 -0300

    change variables names from user_id and lesson_id to user and lesson

--- src/database/migrations/08_lesson_presence.js
+++ src/database/migrations/08_lesson_presence.js
@@ -1,12 +1,20 @@
-exports.up = (knex) =>{
-    return knex.schema.createTable('lesson_presence', (table) => {
-        table.foreign('lesson_id').references('id').inTable('lesson').onDelete('NO ACTION');
-        table.foreign('user_id').references('id').inTable('user').onDelete('NO ACTION');
-        table.primary(['lesson_id', 'user_id']);
-        table.bool('confirmarion').defaultTo(false);
-    })    
-}
+exports.up = (knex) => {
+  return knex.schema.createTable("lesson_presence", (table) => {
+    table
+      .foreign("lesson")
+      .references("id")
+      .inTable("lesson")
+      .onDelete("NO ACTION");
+    table
+      .foreign("user")
+      .references("id")
+      .inTable("user")
+      .onDelete("NO ACTION");
+    table.primary(["lesson", "user"]);
+    table.bool("confirmarion").defaultTo(false);
+  });
+};
 
 exports.down = function (knex) {
-    return knex.schema.dropTable('lesson_presence');
+  return knex.schema.dropTable("lesson_presence");
 };
