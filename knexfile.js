f03bd28ee6fb9349caf86d6f3450c3300ab5538b
Vinicius <viniciusflara@gmail.com>
2021-02-09 23:45:00 -0300

    Database connection

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,45 @@
 // Update with your config settings.
 const path = require('path');
+require('dotenv').config();
 
 module.exports = {
 
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+  //   },
+  //   migrations: {
+  //     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+  //   },
+  //   useNullAsDefault: true
+  // },
+
   development: {
-    client: 'sqlite3',
+    client: 'pg',
+    dialect: "postgres",
     connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+      port: 5432,
+      database: 'd4jprps36s7o0o',
+      user:     'whrvkyvkrteybf',
+      host: 'ec2-3-222-11-129.compute-1.amazonaws.com',
+      password: '24bcaba6b41214493cdbd6ad54d61a8d17935d49763ecd6a7011f4f531fc1779',
+      ssl: true,
+    },
+    dialectOptions: {
+      ssl: true,
+    },
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
-    useNullAsDefault: true
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    },
+    ssl: true
   },
 
   staging: {
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,19 @@
-const knex = require('knex');
-const path = require('path');
+// const knex = require('knex');
+// const path = require('path');
 
-const connection = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: path.resolve(__dirname, 'database.sqlite')
-  },
-  useNullAsDefault: true
-})
+// const connection = knex({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: path.resolve(__dirname, 'database.sqlite')
+//   },
+//   useNullAsDefault: true
+// })
 
-module.exports = connection
+// module.exports = connection
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../knexfile')[environment];
+//console.log(configuration);
+
+const connection = require('knex')(configuration);
+module.exports = connection;
