906dcd68a72a0de61e27fe9be5a1c0c63412d459
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-02-12 09:41:15 -0300

    update paths and add routes

--- src/routes.js
+++ src/routes.js
@@ -17,10 +17,18 @@ const { response } = require("express");
 
 //importação dos controllers
 
-const UserController = require("./controllers/userController");
-const LiveController = require("./controllers/liveController");
-const SessionController = require("./controllers/sessionController");
-const ClassController = require("./controllers/classController");
+const userController = require("./controllers/userController");
+const liveController = require("./controllers/liveController");
+const sessionController = require("./controllers/sessionController");
+const classController = require("./controllers/classController");
+const courseController = require("./controllers/courseController");
+const organizationController = require("./controllers/organizationController");
+const occupationController = require("./controllers/occupationController");
+// const lessonController = require("./controllers/lessonController");
+// const fileController = require("./controllers/fileController");
+// const fileLessonController = require("./controllers/fileLessonController");
+const lessonPresenceController = require("./controllers/lessonPresenceController");
+// const livePresenceController = require("./controllers/livePresenceController");
 
 const {
   authenticateToken,
@@ -33,128 +41,111 @@ routes.get("/", (request, response) => {
   response.send("eae galerinha xdxxdxdxdxd");
 });
 
-// USUÁRIO -------------------------------------------------------------------------------
-
-routes.post(
-  "/newuser",
-  authenticateOptionalToken,
-  celebrate(userValidator.create),
-  UserController.createUser
-);
-routes.delete(
-  "/deleteUser/:user_id",
-  authenticateToken,
-  isMaster,
-  UserController.delete
-);
-routes.put("/user/:user_id", authenticateToken, UserController.updateStudent);
+// ORGANIZATION -------------------------------------------------------------------------
+routes.post("/organization", authenticateToken, organizationController.create);
+routes.get("/organization", authenticateToken, organizationController.read);
 routes.get(
+  "/organization/:id",
+  authenticateToken,
+  organizationController.getById
+);
+routes.put("/organization", authenticateToken, organizationController.update);
+routes.put(
+  "/organization/:id",
+  authenticateToken,
+  organizationController.delete
+);
+
+// OCCUPATION -------------------------------------------------------------------------------
+routes.post("/occupation", authenticateToken, occupationController.create);
+routes.get("/occupation", authenticateToken, occupationController.read);
+routes.get("/occupation/:id", authenticateToken, occupationController.getById);
+routes.get("/occupation", authenticateToken, occupationController.update);
+routes.get("/occupation/:id", authenticateToken, occupationController.delete);
+
+// COURSE -------------------------------------------------------------------------------
+routes.post("/course", authenticateToken, isAdmin, courseController.create);
+routes.get("/course/", authenticateToken, isAdmin, courseController.read);
+routes.get("/course/:id", authenticateToken, isAdmin, courseController.getById);
+routes.delete("/course/:id", authenticateToken, courseController.delete);
+routes.put("/course/:id", authenticateToken, isAdmin, courseController.update);
+
+// CLASS ------------------------------------------------------------------------------
+routes.post("/class", authenticateToken, classController.create);
+routes.get("/class", authenticateToken, classController.read);
+routes.get("/class/:id", authenticateToken, classController.getById);
+routes.put("/class/:id", authenticateToken, classController.update);
+routes.put("/class/:id", authenticateToken, classController.delete);
+
+// // FILE ------------------------------------------------------------------------------
+// routes.post("/file", authenticateToken, fileController.create);
+// routes.get("/file", authenticateToken, fileController.read);
+// routes.get("/file/:id", authenticateToken, fileController.getById);
+// routes.put("/file/:id", authenticateToken, fileController.update);
+// routes.put("/file/:id", authenticateToken, fileController.delete);
+
+// LESSON -------------------------------------------------------------------------------
+// routes.post("/lesson", authenticateToken, lessonController.create);
+// routes.get("/lesson", authenticateToken, lessonController.read);
+// routes.get("/lesson", authenticateToken, lessonController.getById);
+// routes.put("/lesson", authenticateToken, lessonController.update);
+// routes.put("/lesson", authenticateToken, lessonController.delete);
+
+// USER -------------------------------------------------------------------------------
+routes.post("/user", authenticateOptionalToken, userController.create);
+routes.get("/user", authenticateToken, userController.read);
+routes.get("/user/:user_id", authenticateToken, userController.getById);
+routes.put("/user/:user_id", authenticateToken, userController.update);
+routes.put(
   "/user/:user_id",
   authenticateToken,
-  // isAdmin, // isso terá que ser alterado depois. o user deve ser capaz de pegar sua própria informação
-  UserController.getOneUser
-);
-routes.get(
-  "/allstudent",
-  authenticateToken,
-  isAdmin,
-  UserController.getAllStudent
-);
-routes.get(
-  "/alladmin",
-  authenticateToken,
   isMaster,
-  UserController.getAllAdmin
-);
-routes.put(
-  "/promote/:user_id",
-  authenticateToken,
-  isMaster,
-  UserController.promote
-);
-routes.put(
-  "/demote/:user_id",
-  authenticateToken,
-  isMaster,
-  UserController.demote
+  userController.delete
 );
 
 // LIVE -----------------------------------------------------------------------------------
 
-routes.post(
-  "/newlive",
-  authenticateToken,
-  isAdmin,
-  celebrate(liveValidator.create),
-  LiveController.create
-);
-routes.delete(
-  "/deleteLive/:live_id",
-  authenticateToken,
-  isAdmin,
-  celebrate(liveValidator.deletelive),
-  LiveController.delete
-);
-routes.get(
-  "/readlive/:live_id",
-  authenticateToken,
-  isAdmin,
-  LiveController.read
-);
-routes.put(
-  "/updatelive/:live_id",
-  authenticateToken,
-  isAdmin,
-  celebrate(liveValidator.uptade),
-  LiveController.update
-);
+routes.post("/live", authenticateToken, liveController.create);
+routes.delete("/live/:live_id", authenticateToken, liveController.delete);
+routes.get("/live/:live_id", authenticateToken, liveController.getById);
+routes.put("/live/:live_id", authenticateToken, liveController.update);
 
 //SESSION ---------------------------------------------------------------------------------
 
+routes.post("/login", sessionController.signin);
+routes.get("/verify", sessionController.verifyToken);
+routes.post("/forgotpassword", sessionController.forgotPassword);
+
+// PRESENCE-------------------------------------------------------------------------------
+// lesson
 routes.post(
-  "/login",
-  celebrate(sessionValidator.login),
-  SessionController.signin
+  "/lesson/presence",
+  authenticateToken,
+  lessonPresenceController.create
 );
 routes.get(
-  "/verify",
-  celebrate(sessionValidator.verifyToken),
-  SessionController.verifyToken
-);
-routes.post(
-  "/forgotpassword",
-  celebrate(sessionValidator.forgotPassword),
-  SessionController.forgotPassword
-);
-
-// CLASS -----------------------------------------------------------------------------------
-
-routes.post(
-  "/newclass",
+  "/lesson/presence",
   authenticateToken,
-  isAdmin,
-  celebrate(classValidator.create),
-  ClassController.create
-);
-routes.delete(
-  "/deleteClass/:class_id",
-  authenticateToken,
-  isAdmin,
-  celebrate(classValidator.delete),
-  ClassController.delete
-);
-routes.get(
-  "/readclass/:class_id",
-  authenticateToken,
-  isAdmin,
-  ClassController.getById
+  lessonPresenceController.read
 );
 routes.put(
-  "/updateclass/:class_id",
+  "/lesson/presence",
   authenticateToken,
-  isAdmin,
-  celebrate(classValidator.update),
-  ClassController.update
+  lessonPresenceController.update
 );
+routes.delete(
+  "/lesson/presence",
+  authenticateToken,
+  lessonPresenceController.delete
+);
+// live
+// routes.post("/live/presence", authenticateToken, livePresenceController.create);
+// routes.get("/live/presence", authenticateToken, livePresenceController.read);
+// routes.put("/live/presence", authenticateToken, livePresenceController.update);
+// routes.delete(
+//   "/live/presence",
+//   authenticateToken,
+//   livePresenceController.delete
+// );
 
 module.exports = routes;
