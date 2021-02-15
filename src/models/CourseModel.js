ee8cd34ae6f286041d8fd621d6714714456aa69b
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-02-15 20:29:15 -0300

    create getByUserId method for course

--- src/models/CourseModel.js
+++ src/models/CourseModel.js
@@ -8,15 +8,38 @@ module.exports = {
   async getById(id) {
     const response = await connection("course")
       .where({ id })
-      .select("*")
+      .innerJoin("organization", "course.organization_id", "organization.id")
+      .select("course.*", "organization.name as organization")
       .first();
+    return response;
+  },
+  async getByUserId(user_id) {
+    const response = await connection("user_class")
+      .where({ user_id })
+      .andWhereNot("class.is_deleted", "true")
+      .andWhereNot("course.is_deleted", "true")
+      .andWhereNot("user.is_deleted", "true")
+      .join("user", "user.id", "user_class.user_id")
+      .join("organization", "organization.id", "user.organization_id")
+      .join("class", "class.id", "user_class.class_id")
+      .join("course", "course.id", "class.course_id")
+      .select(
+        "organization.id as organization_id",
+        "organization.name as organization_name",
+        "class.id as class_id",
+        "class.name as class_name",
+        "course.id as course_id",
+        "course.name as course_name",
+        "course.description as course_description"
+      );
     return response;
   },
   async read(filters) {
     const response = await connection("course")
       .where(filters)
-      .andWhere({ is_deleted: false })
-      .select("*");
+      .andWhere("course.is_deleted", "false")
+      .innerJoin("organization", "course.organization_id", "organization.id")
+      .select("course.*", "organization.name as organization");
     return response;
   },
   async update(course) {
