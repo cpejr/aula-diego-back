b2345454c277fa3d354d682438c22e2eddf403dd
Vinicius <viniciusflara@gmail.com>
2021-02-17 22:08:17 -0300

    Migrations changes and seeds

--- src/database/migrations/02_user.js
+++ src/database/migrations/02_user.js
@@ -6,6 +6,7 @@ exports.up = (knex) =>{
         table.integer('registration').notNullable();
         table.date('birthdate').notNullable();
         table.string('phone').notNullable();
+        table.string('firebase_id').notNullable();
         table.uuid('organization_id').notNullable();
         table.foreign('organization_id').references('id').inTable('organization').onDelete('NO ACTION');
         table.uuid('occupation_id').notNullable();
