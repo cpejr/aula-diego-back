03415fbabb353e167e86d68a4241f699015b34ba
Paulo Carellos <49373945+paulohcarellos@users.noreply.github.com>
2021-02-22 02:23:07 -0300

    Create class and upload files

--- src/database/migrations/04_class.js
+++ src/database/migrations/04_class.js
@@ -1,18 +1,18 @@
-exports.up = (knex) =>{
-    return knex.schema.createTable('class', (table) => {
-        table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary().notNullable();
-        table.string('name').notNullable();
-        table.text('description', 'longtext');
-        table.uuid('organization_id').notNullable();
-        table.foreign('organization_id').references('id').inTable('organization').onDelete('NO ACTION');
-        table.uuid('course_id').notNullable();
-        table.foreign('course_id').references('id').inTable('course').onDelete('NO ACTION');
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.timestamp('updated_at').defaultTo(knex.fn.now());
-        table.bool('is_deleted').defaultTo(false);
-    })    
+exports.up = (knex) => {
+  return knex.schema.createTable('class', (table) => {
+    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary().notNullable();
+    table.string('name').notNullable();
+    table.text('description', 'longtext');
+    table.uuid('organization_id').notNullable();
+    table.foreign('organization_id').references('id').inTable('organization').onDelete('NO ACTION');
+    table.uuid('course_id').notNullable();
+    table.foreign('course_id').references('id').inTable('course').onDelete('NO ACTION');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.bool('is_deleted').defaultTo(false);
+  })
 }
 
 exports.down = function (knex) {
-    return knex.schema.dropTable('class');
+  return knex.schema.dropTable('class');
 };
--- src/database/migrations/07_file.js
+++ src/database/migrations/07_file.js
@@ -1,15 +1,15 @@
-exports.up = (knex) =>{
-    return knex.schema.createTable('file', (table) => {
-        table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary().notNullable();
-        table.string('path').notNullable();
-        table.uuid('user_id').notNullable();
-        table.foreign('user_id').references('id').inTable('user').onDelete('NO ACTION');
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.timestamp('updated_at').defaultTo(knex.fn.now());
-        table.bool('is_deleted').defaultTo(false);
-    })    
+exports.up = (knex) => {
+  return knex.schema.createTable('file', (table) => {
+    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary().notNullable();
+    table.string('path').notNullable();
+    table.uuid('user_id').notNullable();
+    table.foreign('user_id').references('id').inTable('user').onDelete('NO ACTION');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.bool('is_deleted').defaultTo(false);
+  })
 }
 
 exports.down = function (knex) {
-    return knex.schema.dropTable('file');
+  return knex.schema.dropTable('file');
 };
