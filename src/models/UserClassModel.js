90c07ebb49b91f1f0822d02013f9c77e62c0f78a
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-04-08 11:32:04 -0300

    FIX: improve select

--- src/models/UserClassModel.js
+++ src/models/UserClassModel.js
@@ -12,7 +12,11 @@ module.exports = {
       .andWhere("class.is_deleted", false)
       .join("user", "user_class.user_id", "user.id")
       .join("class", "user_class.class_id", "class.id")
-      .select("class.name as class_name", "user.name as user_name");
+      .select(
+        "class.name as class_name",
+        "user.name as user_name",
+        "class.course_id as course_id"
+      );
     return response;
   },
   async update(userClass) {
