815b4e9ffd3989843544af156dd3d3ad368387dc
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-12-13 08:31:00 -0300

    refactor: update lessonPresenceController response messages

--- src/controllers/lessonPresenceController.js
+++ src/controllers/lessonPresenceController.js
@@ -13,10 +13,12 @@ module.exports = {
       const user = await UserModel.getById(lessonPresence.user_id);
       await UserModel.update(lessonPresence.user_id, { score: user.score + 1 });
 
-      return response.status(200).json("Presença em lição criada com succeso!");
+      return response
+        .status(200)
+        .json({ message: "Presença em lição criada com succeso!" });
     } catch (error) {
       console.warn(error.message);
-      response.status(500).json("internal server error");
+      response.status(500).json({ message: "Internal server error." });
     }
   },
 
@@ -27,7 +29,7 @@ module.exports = {
       return response.status(200).json(result);
     } catch (error) {
       console.warn(error);
-      response.status(500).json("internal server error");
+      response.status(500).json({ message: "Internal server error." });
     }
   },
 
@@ -39,20 +41,24 @@ module.exports = {
       if (loggedUser.id != lessonPresence.user && loggedUser.type != "master")
         return response
           .status(403)
-          .json("Você não tem permissão para realizar esta operação");
+          .json({
+            message: "Você não tem permissão para realizar esta operação",
+          });
 
       const res = await LessonPresenceModel.update(lessonPresence);
 
       if (res !== 1) {
-        return response.status(404).json("Presença em lição não encontrada!");
+        return response
+          .status(404)
+          .json({ message: "Presença em lição não encontrada!" });
       } else {
         return response
           .status(200)
-          .json("Presença em lição alterada com sucesso ");
+          .json({ message: "Presença em lição alterada com sucesso" });
       }
     } catch (error) {
       console.log(error.message);
-      return response.status(500).json("internal server error ");
+      return response.status(500).json({ message: "Internal server error." });
     }
   },
 
@@ -61,10 +67,12 @@ module.exports = {
       const { filters } = request.query;
 
       const result = await LessonPresenceModel.delete(filters);
-      response.status(200).json("Presença em lição apagada com sucesso!");
+      response
+        .status(200)
+        .json({ message: "Presença em lição apagada com sucesso!" });
     } catch (error) {
       console.warn(error.message);
-      response.status(500).json("internal server error ");
+      response.status(500).json({ message: "Internal server error." });
     }
   },
 };
